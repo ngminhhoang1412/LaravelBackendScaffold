3006db5e0f60bc240504d78a6742919c66e4554b
dungmoli <dung.nguyen@molinetwork.com>
2023-04-14 17:42:38 +0700

    update redirect

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\LinkController;
 use App\Http\Middleware\AuthStore;
 use App\Models\User;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
@@ -19,11 +20,14 @@ use App\Http\Controllers\AuthController;
 
 Route::post('auth/register', [AuthController::class, 'createUser']);
 Route::post('auth/login', [AuthController::class, 'loginUser']);
-Route::middleware(['auth:sanctum', AuthStore::class])->group(function () {
+Route::middleware([])->group(function () {
         Route::get('link', [LinkController::class, 'index']);
+        Route::get('user/link', [LinkController::class, 'getByUser']);
         Route::get('link/{id}', [LinkController::class, 'show']);
         Route::post('link', [LinkController::class, 'createLink']);
         Route::put('link/{id}', [LinkController::class, 'handleUpdate']);
         Route::delete('link/{id}', [LinkController::class, 'destroy']);
+        Route::get('user', [LinkController::class, 'getUser']);
 });
+
 
