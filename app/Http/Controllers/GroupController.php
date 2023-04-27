f4408b58a215e90900d3738d9f08e3a707e2eb9e
thuan122 <127835141+thuan122@users.noreply.github.com>
2023-04-27 15:53:19 +0700

    Update GroupController.php

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,15 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
-use App\Models\Link;
-use App\Models\User;
 use App\Models\Group;
-use App\Common\Helper;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use App\Common\GlobalVariable;
-use Illuminate\Support\Facades\DB;
 
 class GroupController extends Controller
 {
