3b2102335751a67561a40900df55bbc6805620fb
dungmoli <dung.nguyen@molinetwork.com>
2023-05-09 17:34:07 +0700

    migarate and seed channels and teams

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\TeamController;
 use App\Http\Middleware\AuthStore;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -23,6 +26,15 @@ Route::post('auth/login', [AuthController::class, 'loginUser']);
 Route::middleware(['auth:sanctum', AuthStore::class])->group(function () {
     Route::resource('roles', RoleController::class);
     Route::resource('permissions', PermissionController::class);
+    Route::post('groups', [GroupController::class, 'store']);
+    Route::get('groups', [GroupController::class, 'index']);
+    Route::delete('groups/{id}', [GroupController::class, 'destroy']);
+    Route::post('categories', [CategoryController::class, 'store']);
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+    Route::post('teams', [TeamController::class, 'store']);
+    Route::get('teams', [TeamController::class, 'index']);
+    Route::delete('teams/{id}', [TeamController::class, 'destroy']);
     Route::post('permission/assign', [PermissionController::class, 'assignPermissionToRole']);
     Route::post('role/assign', [RoleController::class, 'assignRoleToUser']);
 });
