57107298275dc33684964d41a5ac0d5c58576c7a
thuan122 <127835141+thuan122@users.noreply.github.com>
2023-05-15 11:19:13 +0700

    Update AbsenceController.php

--- app/Http/Controllers/AbsenceController.php
+++ app/Http/Controllers/AbsenceController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Common\Helper;
 use App\Models\AbsenceType;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
 
 class AbsenceController extends Controller
 {
