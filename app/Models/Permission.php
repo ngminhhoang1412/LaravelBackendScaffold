9671d4aff853ab14641cbe9266569cb0a599e7c1
thuan122 <127835141+thuan122@users.noreply.github.com>
2023-05-25 11:10:49 +0700

    get create and edit validate
    permission and role get store validate fixed

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -27,7 +27,7 @@ class Permission extends BaseModel
 
     protected $guard = 'web';
 
-    static function getInsertValidator(Request $request): array
+    static function getStoreValidator(Request $request): array
     {
         return array_merge(
             [
@@ -39,7 +39,7 @@ class Permission extends BaseModel
                     'string'
                 ]
             ],
-            parent::getInsertValidator($request)
+            parent::getStoreValidator($request)
         );
     }
 
